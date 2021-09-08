a92c5ff86bc962a0ad56cfb1425534df77562460
João Marcelo Vieira <joaonininho.twt@gmail.com>
2021-09-08 11:45:17 -0300

    first tests

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+describe("ActiveLink component", () => {
+  it("active link renders correctly", () => {
+    render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>home</a>
+      </ActiveLink>
+    );
+    expect(screen.getByText("Home")).toBeInTheDocument();
+  });
+
+  it("active link is receiving active class", () => {
+    render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>home</a>
+      </ActiveLink>
+    );
+    expect(screen.getByText("Home")).toHaveClass("active");
+  });
+});
